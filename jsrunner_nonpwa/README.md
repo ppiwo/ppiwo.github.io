1f73d0881519cc0bb2ddc652a1f964ac86e9573e
Patrick Piwowarczyk <piwowarczykpatrick@gmail.com>
2020-01-22 22:49:02 -0600

    Add files via upload

---
+++ jsrunner_nonpwa/README.md
@@ -0,0 +1,7 @@
+# IT338
+
+HOW TO USE:
+
+Run the .html file on a web server. To test the sandbox use the following JS code:
+document.getElementById("resultArea").innerHTML = toCelsius(68)
+function toCelsius(fahrenheit) { return(5/9) * (fahrenheit-32);}
---
+++ jsrunner_nonpwa/_tocelsius.js
@@ -0,0 +1,15 @@
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+}
+document.getElementById("resultArea").innerHTML = toCelsius(68);
+alert (toCelsius(68));
+// answer in celsius is 20
+
+
+document.getElementById(“resultArea”).innerHTML = toCelsius(68)
+function toCelsius(fahrenheit) { return(5/9) * (fahrenheit-32);}
+
+
+
+document.getElementById("resultArea").innerHTML = toCelsius(68)
+function toCelsius(fahrenheit) { return(5/9) * (fahrenheit-32);}
